4cd9d650dc05c6ecba15fceac8484393dfca17a5
nictunz <kpj3669@gmail.com>
2026-08-24 00:50:45 +0900

    feat: export hybrid crypto market adapter

--- universal_bot/adapters/__init__.py
+++ universal_bot/adapters/__init__.py
@@ -1,4 +1,5 @@
 from universal_bot.adapters.ccxt_adapter import CCXTAdapter
+from universal_bot.adapters.hybrid_ccxt_adapter import HybridCCXTAdapter
 from universal_bot.adapters.stock import YFinanceMarketAdapter
 
-__all__ = ["CCXTAdapter", "YFinanceMarketAdapter"]
+__all__ = ["CCXTAdapter", "HybridCCXTAdapter", "YFinanceMarketAdapter"]
