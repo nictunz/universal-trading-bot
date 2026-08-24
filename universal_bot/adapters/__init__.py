1542e5a8a3eaa17174f07b3d94c445ce5f1ca64f
nictunz <kpj3669@gmail.com>
2026-08-25 01:30:50 +0900

    feat: export Bitget Elite adapter

--- universal_bot/adapters/__init__.py
+++ universal_bot/adapters/__init__.py
@@ -1,5 +1,6 @@
+from universal_bot.adapters.bitget_elite import BitgetEliteAdapter
 from universal_bot.adapters.ccxt_adapter import CCXTAdapter
 from universal_bot.adapters.hybrid_ccxt_adapter import HybridCCXTAdapter
 from universal_bot.adapters.stock import YFinanceMarketAdapter
 
-__all__ = ["CCXTAdapter", "HybridCCXTAdapter", "YFinanceMarketAdapter"]
+__all__ = ["BitgetEliteAdapter", "CCXTAdapter", "HybridCCXTAdapter", "YFinanceMarketAdapter"]
