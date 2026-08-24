3e0019a736de632f0fce1f70fe229c94712e8332
nictunz <kpj3669@gmail.com>
2026-08-25 08:13:43 +0900

    feat: route Elite runtime through leverage-enforcing adapter

--- universal_bot/adapters/__init__.py
+++ universal_bot/adapters/__init__.py
@@ -1,4 +1,4 @@
-from universal_bot.adapters.bitget_elite import BitgetEliteAdapter
+from universal_bot.adapters.bitget_elite_runtime import BitgetEliteAdapter
 from universal_bot.adapters.ccxt_adapter import CCXTAdapter
 from universal_bot.adapters.hybrid_ccxt_adapter import HybridCCXTAdapter
 from universal_bot.adapters.stock import YFinanceMarketAdapter
